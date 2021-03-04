7b28ce3c2f63dd38d9af0504620dcdb0bd30976a
Belhaver <belhaver@gmail.com>
2021-03-04 19:35:10 +0100

    Update ContactCodeService.dart

--- lib/contactcode/ContactCodeService.dart
+++ lib/contactcode/ContactCodeService.dart
@@ -2,6 +2,12 @@ import 'package:contact_app_flutter/AppConstans.dart';
 import 'package:contact_app_flutter/security/TokenRepository.dart';
 import 'package:http/http.dart' as http;
 
+//it should be a class ContactCodeService or ContactCodeApiClient
+
+// you should add another layer between view (or presenter) and api client i.e. ContactCodeInteractor (please read what interactors should do). 
+// This way you will implement code closer to SOLID principles. 
+// ApiClient should just have methods below and interactor should wrap data you are getting into a form ready to save in database / display / etc
+
 Future<String> getContactCode() async {
   final response = await http.post(
     Uri.http(AppConstans.baseUrl, '/api/contactCode/create'),
